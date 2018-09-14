00d9f07f0565bb96abf5bf62c4775dceaa492853
Namrata Patnaik <npatnaik@braodsoft.com>
2018-09-14 20:45:39 +0530

    initial commit

---
+++ web/src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { GetGoogleFonts } from 'get-google-fonts';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+
+//new GetGoogleFonts().download('https://fonts.googleapis.com/css?family=Roboto:400,700&subset=cyrillic');
+
