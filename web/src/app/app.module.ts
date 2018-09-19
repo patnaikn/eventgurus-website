5cc033aff63dc645df65fe4b6fcacfb06c606c90
Namrata Patnaik <npatnaik@braodsoft.com>
2018-09-19 17:02:14 +0530

    added more components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,10 +10,28 @@ import { CountUpModule } from 'countup.js-angular2';
 // import 'imports?jQuery=jquery!owl.carousel';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { TeamComponent } from './team/team.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { StatsComponent } from './stats/stats.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    WorkComponent,
+    TeamComponent,
+    TestimonialsComponent,
+    PricingComponent,
+    StatsComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
---
+++ web/src/app/stats/stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.css','../app.component.css']
+})
+export class StatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
